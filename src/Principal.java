92c935b2110277fe92e2fdc4b78529647678e7ac
LuzSelene2020 <71992211+LuzSelene2020@users.noreply.github.com>
2020-11-24 07:39:42 -0300

    agrego Proyecto 9

--- src/Principal.java
+++ src/Principal.java
@@ -1,36 +1,51 @@
-
+import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);	
 		// TODO Auto-generated method stub
 // Aquí voy a poner mi código
-	Proyecto8bis();
+	Proyecto9();
 	}
-	public static void Proyecto8bis() {
+	public static void Proyecto9() {
+		Scanner sc = new Scanner(System.in);
+		
+		long acumulador = 0;
+		int numeroA;
+		
+		try
+		{
+			
+			
+			System.out.println("Ingrese varios números enteros, y un número negativo para finalizar");
+			
+			
+	
+			numeroA = sc.nextInt();
+		
+		  	
+		    while ( numeroA>= 0) {
+		    	acumulador = acumulador + numeroA;
+		    	numeroA = sc.nextInt();
+		    	
+		}
+		    System.out.println("La suma es:" + acumulador);
+		   
+		}
+		catch(Exception ex)
+		{ 
+			System.out.println("Ingresó un valor incorrecto");	
+		}
+
+		    
+		
+		
+		
+		
+	}
 		
 
-	
-				
-		int i;
-        int a = 4;
-        
-        
-        
-        
-       
-				
-				for (i = 0; i<= 200; i= i +2)
-				
-					
-				
-				
-				
-					if ((i/2) <= 200  && (i%a) == 0 && i > 0 )
-        
-				
-        System.out.println("Númewro múltipo de 4:"    + i);  			
-			  				
-	
-				} 
-
 	}
+
+
