64afb67d85dfa43f219e616c10dcbe99bbe2a65a
LuzSelene2020 <71992211+LuzSelene2020@users.noreply.github.com>
2020-11-24 07:16:17 -0300

    Agrero ej. 8bis

--- src/Principal.java
+++ src/Principal.java
@@ -4,21 +4,33 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 // Aquí voy a poner mi código
-	Proyecto7bis();
+	Proyecto8bis();
 	}
-	public static void Proyecto7bis() {
+	public static void Proyecto8bis() {
 		
-int i;
 
 	
-		
-		for (i = 0; i<= 200; i= i +2)
-		  	
-	  		if ((i/2) <= 200)	
-		
-			
+				
+		int i;
+        int a = 4;
+        
+        
+        
+        
+       
+				
+				for (i = 0; i<= 200; i= i +2)
+				
+					
+				
+				
+				
+					if ((i/2) <= 200  && (i%a) == 0 && i > 0 )
+        
+				
+        System.out.println("Númewro múltipo de 4:"    + i);  			
+			  				
 	
-	System.out.println(i);
-}
-}
+				} 
 
+	}
