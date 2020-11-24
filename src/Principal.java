9d54cc86e712d52dd651bf3fde8297bc8e405bb8
LuzSelene2020 <71992211+LuzSelene2020@users.noreply.github.com>
2020-11-24 07:43:30 -0300

    agrego proyecto 9bis

--- src/Principal.java
+++ src/Principal.java
@@ -6,46 +6,43 @@ public class Principal {
 		Scanner sc = new Scanner(System.in);	
 		// TODO Auto-generated method stub
 // Aquí voy a poner mi código
-	Proyecto9();
+	Proyecto9bis();
 	}
-	public static void Proyecto9() {
+	public static void Proyecto9bis() {
 		Scanner sc = new Scanner(System.in);
-		
-		long acumulador = 0;
-		int numeroA;
 		
 		try
 		{
-			
-			
+		
 			System.out.println("Ingrese varios números enteros, y un número negativo para finalizar");
 			
-			
+			long acumulador = 0;
+			int numeroA;
 	
+			
+		do
+		{
 			numeroA = sc.nextInt();
+		if (numeroA > 0)
 		
-		  	
-		    while ( numeroA>= 0) {
-		    	acumulador = acumulador + numeroA;
-		    	numeroA = sc.nextInt();
-		    	
+		{
+			acumulador = acumulador + numeroA;
 		}
+		} while ( numeroA>= 0); 
+		    	
 		    System.out.println("La suma es:" + acumulador);
+		
 		   
+		
 		}
 		catch(Exception ex)
 		{ 
 			System.out.println("Ingresó un valor incorrecto");	
 		}
 
-		    
-		
-		
-		
-		
-	}
-		
+
 
 	}
 
+}
 
